e1a3484f27477770a13e06f4b06ba75d800d0fb3
okpierre <1679025+okpierre@users.noreply.github.com>
2019-10-04 18:54:58 -0400

    Create site.php for KO
    translations for KO

---
+++ resources/lang/ko/site.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'about'             => '정보',
+    'help'              => '지원',
+    'language'          => '언어',
+    'fediverse'         => 'Fediverse',
+    'opensource'        => 'Open Source',
+    'terms'             => '약관',
+    'privacy'           => '개인정보처리방침',
+    'l10nWip'           => '우리는 여전히 현지화 지원을 위해 노력하고 있습니다',
+    'currentLocale'     => '현재 로케일',
+    'selectLocale'      => '지원되는 언어 중 하나를 선택하십시오',
+    'contact'           => '문의하기',
+    'contact-us'        => '문의하기',
+    'places'            => '디렉터리',
+];
